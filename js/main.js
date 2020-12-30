8810097d379517fa35b9ee23f921a6b86856f3f4
Stanimira Atanasova <sta.atanasova@gmail.com>
2020-12-30 15:13:49 +0200

    nav accessibility

--- js/main.js
+++ js/main.js
@@ -5,11 +5,20 @@ const hamburgerIcon = document.querySelector('#hamburgerIcon');
 const closeIcon = document.querySelector('#closeIcon');
 const navWrap = document.querySelector('#navWrap');
 const overlay = document.querySelector('#overlay');
+const nav = document.querySelector('#headerNav');
 
 function menuHandler() {
     navWrap.classList.toggle('open');
     overlay.classList.toggle('open');
     body.classList.toggle('no-overflow');
+
+    let expanded = hamburgerIcon.getAttribute("aria-expanded"); 
+    expanded = (expanded == "true") ? "false" : "true";
+    
+    hamburgerIcon.setAttribute("aria-expanded", expanded);
+    closeIcon.setAttribute("aria-expanded", expanded);
+
+
 }
 
 hamburgerIcon.addEventListener('click', menuHandler);
